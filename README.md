50e0ca1c0419b425398af471db6953c62daddbd8
Panchananturuk1 <pturuk123@gmail.com>
2022-04-27 17:55:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hjaexe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hjaexe)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+
+
+
+  userData = new FormGroup({
+    fullname: new FormControl('', [Validators.required]),
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confpassword: new FormControl('', [Validators.required]),
+  });
+
+
+
+}
